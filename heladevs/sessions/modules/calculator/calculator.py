c5394afdb30018ed89de77ed9b31f2b96a2f3e4f
kanishkasubash <kanishkasubash@gmail.com>
2023-04-29 21:41:04 +0530

    Update calculator.py

---
+++ heladevs/sessions/modules/calculator/calculator.py
@@ -0,0 +1,9 @@
+import calcutil as cal
+
+x = 25
+y = 40
+
+print(f"Sum : {cal.add(x, y)}")
+print(f"Subtract : {cal.sub(x, y)}")
+print(f"Multiplication : {cal.mul(x, y)}")
+print(f"Division : {cal.div(x, y)}")
