c9c785d6167c0b7cd2ffdcb733febfa756c5c117
kanishkasubash <kanishkasubash@gmail.com>
2023-05-02 04:00:45 +0530

    Update string_count.py

--- heladevs/exercises/functions_ex/string_count.py
+++ heladevs/exercises/functions_ex/string_count.py
@@ -1,5 +1,5 @@
 import stringutil
 
 fruits = ["apple", "orange", "banana", "avocado"]
-count = stringutil.count_strings_by_character(fruits, "b")
+count = stringutil.count_strings_by_character(fruits, "B")
 print(f"Count : {count}")
