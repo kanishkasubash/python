d9f3deedeec340ab421ae45d9274e768ffac103e
kanishkasubash <kanishkasubash@gmail.com>
2023-05-02 04:00:51 +0530

    Update stringutil.py

--- heladevs/exercises/functions_ex/stringutil.py
+++ heladevs/exercises/functions_ex/stringutil.py
@@ -1,7 +1,9 @@
-def count_strings_by_character(list, character) -> int:
+def count_strings_by_character(strings, character) -> int:
     """Return the count of strings starting with the given character"""
     count = 0
-    for string in list:
-        if string.startswith(character):
+    cap_char = character.capitalize()
+    for string in strings:
+        cap_string = string.capitalize()
+        if cap_string.startswith(cap_char):
             count += 1
     return count
