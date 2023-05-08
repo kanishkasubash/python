88bc4ac5ba6b1dbf48f6cc0a69157705c1e951dc
kanishkasubash <kanishkasubash@gmail.com>
2023-05-08 11:30:59 +0530

    Update lab04_ex_01.py

--- heladevs/exercises/lab04_ex/lab04_ex_01.py
+++ heladevs/exercises/lab04_ex/lab04_ex_01.py
@@ -1,8 +1,8 @@
 # Print odd numbers from 1 to 30 using a loop
 odd_numbers = []
-for number in range(0, 30):
+for number in range(1, 31):
     val = number % 2
-    if val != 0:
+    if val == 1:
         odd_numbers.append(number)
 
 print("Odd Numbers from 1 to 30 : ", odd_numbers)
