a6a59d85fa46e3c8890415fdcd0f5dbd31531d59
kanishkasubash <kanishkasubash@gmail.com>
2023-08-29 13:24:21 +0530

    new file:   .gitignore 	new file:   heladevs/labs/lab-08/todolistapp.py 	new file:   heladevs/labs/lab-08/todolistutil.py

---
+++ heladevs/labs/lab-08/todolistutil.py
@@ -0,0 +1,9 @@
+def display_menu():
+    print("Simple TODO List")
+    print("0 - Exit")
+    print("1 - Display TODO List")
+    print("2 - Add Task to TODO List")
+    print("3 - Mark Task as complete")
+    print("4 - Mark Task as incomplete")
+    print("5 - Delete Task")
+    
